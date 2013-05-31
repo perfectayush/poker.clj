d72a10b0c0fef33a343cbb118557788f5b69c347
Ayush Goyal <ayush.go@directi.com>
2013-05-31 22:23:11 +0530

    update clojure version to 1.5.0

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Mozilla Public License"
             :url "http://www.mozilla.org/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.combinatorics "0.0.4"]])
